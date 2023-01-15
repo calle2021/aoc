9d9ca569301ccd5cc9ce26a33c11ecd85aa3c204
Calle <calle.janlenstenberg@gmail.com>
2023-01-15 10:55:28 +0100

    solution d15

--- d15/d15.py
+++ d15/d15.py
@@ -3,10 +3,11 @@ lst = [0,13,1,8,6,15]
 last_idx = {}
 for i, n in enumerate(lst):
         last_idx[n] = i
+
 turn = 2020 #part 1
 turn = 30000000 #part 2
 
-while len(lst) < 2020:
+while len(lst) < turn:
     prev = lst[-1]
     prev_idx = last_idx.get(prev, -1)
     last_idx[prev] = len(lst) - 1
