a551335511329bdde69e4e67ba5555d321585465
unknown <calle.janlenstenberg@gmail.com>
2023-03-02 07:57:19 +0100

    solution part1 d5

---
+++ d5/d5_part1.py
@@ -0,0 +1,38 @@
+infile = "ex.txt"
+
+data = []
+for line in open(infile):
+    line = line.strip()
+    line = line.replace(" -> ", ",")
+    line = [int(l) for l in line.split(",")]
+    data.append(line)
+
+def interval(start, end):
+    if start < end:
+        return [n for n in range(start, end + 1)]
+    else:
+        return [n for n in range(end, start + 1)]
+
+d = {}
+for l in data:
+    if l[0] != l[2] and l[1] != l[3]:
+        continue
+    
+    points = []
+    if l[0] == l[2]:
+        i = interval(l[1], l[3])
+        points = [(l[0], y) for y in i]
+    elif l[1] == l[3]:
+        i = interval(l[0], l[2])
+        points = [(x, l[1]) for x in i]
+        
+    for p in points:
+        if p in d:
+            d[p] += 1
+        else:
+            d[p] = 1
+sum = 0
+for d_ in d.values():
+    if d_ > 1:
+        sum += 1
+print(sum)
