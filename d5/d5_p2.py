48bdfd2d28fafeccec4d55021230f8d4624e3135
Calle Stenberg <calle.janlenstenberg@gmail.com>
2023-12-05 08:26:33 +0100

    Update d5 p2

--- d5/d5_p2.py
+++ d5/d5_p2.py
@@ -4,7 +4,7 @@ input = "ex0.txt"
 almanac = [d.strip() for d in open(input).read().split("\n\n")]
 
 data = list(map(int, re.findall("\d+", almanac.pop(0).split("seeds: ")[1])))
-seeds = [(x, y) for x, y in zip(data[::2], data[1::2])]
+seeds = [(x, x + y) for x, y in zip(data[::2], data[1::2])]
 
 maps = []
 for a in almanac:
@@ -17,18 +17,32 @@ for a in almanac:
 ## soil, fert, wat, light, temp, hum , loc
 # 50 98 2 dest range, source range, range length
 
-def inside(x, d, s ,l):
+def inside(x, s ,l):
     return s <= x <= s + l
 
 # idea split current range up in smaller ranges that fit the correct range, and add thsoe to new seeds
 for m in maps:
     new_seeds = []
-
     while seeds:
         start, end = seeds.pop(0)
         for d, s, l in m:
-            i, res = inside(start, d, s, l)
-            if not res:
-                seeds.append((start,end))
-            start_dest = d + start - s
-            
+            if not inside(start,s, l):
+                continue
+
+            start_dest = d + abs(start - s)
+            max_s = s + l
+            if max_s >= end:
+                end_dest = d + abs(end - s)
+                new_seeds.append((start_dest, end_dest))
+                break
+            else:
+                seeds.append((max_s, end))
+                break
+        else:
+            new_seeds.append((start, end))
+
+    seeds = new_seeds
+    print(seeds)
+
+print(seeds)
+
