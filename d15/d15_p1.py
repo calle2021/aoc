fc60bed9b65f77edcbb99fed3a4670be7c13c7a5
Calle Stenberg <calle.janlenstenberg@gmail.com>
2023-12-16 11:30:06 +0100

    d15 cleanup

--- d15/d15_p1.py
+++ d15/d15_p1.py
@@ -2,16 +2,14 @@ input = "input.txt"
 
 seq = [l.strip() for l in open(input).read().split(",")]
 
-def hash(label):
+def hash(init):
     value = 0
-    for l in label:
-        value += ord(l)
+    for i in init:
+        value += ord(i)
         value *= 17
         value %= 256
     return value
 
-res = 0
-for init in seq:
-    res += hash(init)
+res = sum([hash(init) for init in seq])
 #part 1
 print(res)
--- d15/d15_p2.py
+++ d15/d15_p2.py
@@ -2,7 +2,6 @@ input = "input.txt"
 
 seq = [l.strip() for l in open(input).read().split(",")]
 
-
 boxes = [[] for _ in range(256)]
 indicies = {}
 
@@ -18,16 +17,15 @@ for op in seq:
     if "=" in op:
         label, focal_length = op.split("=")
         focal_length = int(focal_length)
-        i = hash(label)
 
         if label in indicies:
             curr = indicies[label]
             boxes[curr] = [[label, focal_length] if x[0] == label else x for x in boxes[curr]]
         else:
+            i = hash(label)
             boxes[i].append([label, focal_length])
             indicies[label] = i
     if "-" in op:
-        i = hash(label)
         label = op.split("-")[0]
         if label in indicies:
             curr = indicies[label]
@@ -35,9 +33,7 @@ for op in seq:
             del indicies[label]
 res = 0
 for i, b in enumerate(boxes):
-    if not b:
-        continue
-    for j, lens in enumerate(b):
-        res += (i + 1) * (j + 1) * lens[1]
+    if not b: continue
+    res += sum((i + 1) * (j + 1) * lens[1] for j, lens in enumerate(b))
 #part 2
 print(res)
