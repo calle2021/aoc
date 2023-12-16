a150c0f035c7123ef7ecde8cc0d356b3657b01bb
Calle Stenberg <calle.janlenstenberg@gmail.com>
2023-12-16 16:16:34 +0100

    d13 cleanup

--- d13/d13_p1.py
+++ d13/d13_p1.py
@@ -1,59 +1,30 @@
+import numpy as np
 input = "input.txt"
 
 patterns = open(input).read().split("\n\n")
 
-
-def reflection(ash, rock, mx, my):
-
-    for row in range(0, my): # reference horizontal line
-        top = set()
-        bottom = set()
+def reflect(grid, mx, my):
+    for row in range(0, my):
         dy = min(my - row - 1, row + 1)
-        for y in range(row - dy + 1, row + 1):
-            for x in range(0, mx):
-                if (x + y * 1j) in rock:
-                    top.add(x + (row  + 1 - y) * 1j)
-        for y in range(row + 1, row + dy + 1):
-            for x in range(0, mx):
-                if (x + y * 1j) in rock:
-                    bottom.add(x + (y - row) * 1j)
-
-        if top == bottom and len(top) != 0 and len(bottom) != 0:
+        top = grid[row - dy + 1: row + 1, :]
+        bottom = np.flip(grid[row + 1: row + dy + 1, :], axis=0)
+        if top.size == 0 or bottom.size == 0: continue
+        if np.array_equal(top, bottom):
             return (row + 1) * 100
-
-    for col in range(0, mx): # reference vertical line
-        left = set()
-        right = set()
+    
+    for col in range(0, mx):
         dx = min(mx - col - 1, col + 1)
-        for x in range(col - dx + 1 , col + 1):
-            for y in range(0, my):
-                if (x + y * 1j) in rock:
-                    left.add((col  + 1 - x) + y * 1j)
-        for x in range(col + 1, col + dx + 1):
-            for y in range(0, my):
-                if (x + y * 1j) in rock:
-                    right.add((x - col) + y * 1j)
-
-        if left == right and len(left) != 0 and len(right) != 0:
+        left = grid[:, col - dx + 1: col + 1]
+        right = np.flip(grid[:, col + 1 : col + dx + 1], axis=1)
+        if left.size == 0 or left.size == 0: continue
+        if np.array_equal(left, right):
             return col + 1
-
-
-pattern = []
-s = 0
+        
+res = 0
 for p in patterns:
-    ash = []
-    rocks = []
-    my = 0
-    mx = 0
-    for y, row in enumerate(p.split("\n")):
-        my = max(my, y)
-        for x, v in enumerate(row):
-            mx = max(mx, x)
-            if v == "#":
-                rocks.append(x + y * 1j)
-            else:
-                ash.append(x + y * 1j)
-    res = reflection(ash, rocks, mx + 1, my + 1)
-    s += res
-
-print(s)
+    grid = np.array([[ord(x) for x in y] for y in p.split("\n")])
+    mx = len(grid[0, :])  
+    my = len(grid[:, 0])
+    res += reflect(grid, mx, my)
+#part 1
+print(res)
--- d13/d13_p2.py
+++ d13/d13_p2.py
@@ -1,92 +1,30 @@
+import numpy as np
 input = "input.txt"
 
 patterns = open(input).read().split("\n\n")
 
-def reflection(rock, mx, my, res=0):
-    for row in range(0, my): # reference horizontal line
-        top = set()
-        bottom = set()
+def reflect(grid, mx, my):
+    for row in range(0, my):
         dy = min(my - row - 1, row + 1)
-        for y in range(row - dy + 1, row + 1):
-            for x in range(0, mx):
-                if (x + y * 1j) in rock:
-                    top.add(x + (row  + 1 - y) * 1j)
-        for y in range(row + 1, row + dy + 1):
-            for x in range(0, mx):
-                if (x + y * 1j) in rock:
-                    bottom.add(x + (y - row) * 1j)
-
-        if top == bottom and top and bottom and res != (row + 1) * 100:
+        top = grid[row - dy + 1: row + 1, :]
+        bottom = np.flip(grid[row + 1: row + dy + 1, :], axis=0)
+        if top.size == 0 or bottom.size == 0: continue
+        if np.count_nonzero(top != bottom) == 1:
             return (row + 1) * 100
-
-    for col in range(0, mx): # reference vertical line
-        left = set()
-        right = set()
-        dx = min(mx - col - 1, col + 1)
-        for x in range(col - dx + 1 , col + 1):
-            for y in range(0, my):
-                if (x + y * 1j) in rock:
-                    left.add((col  + 1 - x) + y * 1j)
-        for x in range(col + 1, col + dx + 1):
-            for y in range(0, my):
-                if (x + y * 1j) in rock:
-                    right.add((x - col) + y * 1j)
-
-        if left == right and left and right and res != col + 1:
-            return col + 1
-
-    return None
-
-def replace_rock(rocks, res):
-    for _ in range(len(rocks)):
-        r = rocks.pop(0)
-        new = reflection(rocks, mx, my, res)
-        rocks.append(r)
-        if not new:
-            continue
-        return new
-    else:
-        return None
-
-def replace_ash(ash, rocks, res):
-    for _ in range(len(ash)):
-        a = ash.pop(0)
-        rocks.append(a)
-        new = reflection(rocks, mx, my, res)
-        rocks.pop()
-        if not new:
-            continue
-        return new
-    else:
-        return None
-
-
-pattern = []
-s = 0
-for i, p in enumerate(patterns):
-    ash = []
-    rocks = []
-    my = 0
-    mx = 0
-    for y, row in enumerate(p.split("\n")):
-        my = max(my, y)
-        for x, v in enumerate(row):
-            mx = max(mx, x)
-            if v == "#":
-                rocks.append(x + y * 1j)
-            else:
-                ash.append(x + y * 1j)
-    mx += 1
-    my += 1
-    res = reflection(rocks, mx, my)
     
-    smudge = replace_rock(rocks[:], res)
-    if smudge == None:
-        smudge = replace_ash(ash[:], rocks[:], res)
-
-    if smudge == None:
-        print(p)
-    else:
-        s += smudge
-
-print(s)
+    for col in range(0, mx):
+        dx = min(mx - col - 1, col + 1)
+        left = grid[:, col - dx + 1: col + 1]
+        right = np.flip(grid[:, col + 1 : col + dx + 1], axis=1)
+        if left.size == 0 or left.size == 0: continue
+        if np.count_nonzero(left != right) == 1:
+            return col + 1
+        
+res = 0
+for p in patterns:
+    grid = np.array([[ord(x) for x in y] for y in p.split("\n")])
+    mx = len(grid[0, :])  
+    my = len(grid[:, 0])
+    res += reflect(grid, mx, my)
+#part 2
+print(res)
