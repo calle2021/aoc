de2a6a36afd6c8a66affc3b2ff30a3779ab66738
calle2021 <calle.janlenstenberg@gmail.com>
2024-11-14 20:53:27 +0100

    Added solution part 1 day 16 2019

---
+++ aoc-2019/d16.py
@@ -0,0 +1,31 @@
+from aocd.models import Puzzle
+from math import ceil
+year = 2019
+day = 16
+puzzle = Puzzle(year=year, day=day)
+
+input_signal = puzzle.input_data
+
+signal = [int(x) for x in input_signal]
+
+patterns = []
+base = [0, 1, 0, -1]
+curr = [0, 1, 0, -1]
+for depth in range(len(input_signal)):
+    new = []
+    for c in base:
+        new += [c] * (depth + 1)
+    curr = new
+    patterns.append(curr)
+
+phases = 100
+for _ in range(phases):
+    new_signal = []
+    for depth in range(len(signal)):
+        pattern = patterns[depth]
+        pattern = pattern * ceil((len(signal) + 1) / len(pattern))
+        pattern = pattern[1:len(signal) + 1]
+        s = abs(sum([x * y for x, y in zip(signal, pattern)])) % 10
+        new_signal.append(s)
+    signal = new_signal
+print(''.join(map(str, signal[:8])))
