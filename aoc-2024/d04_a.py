46d633da7cfb3e728c067f9cbe30710961ceb1fa
calle2021 <calle.janlenstenberg@gmail.com>
2024-12-04 07:30:46 +0100

    cleaned up

--- aoc-2024/d04_a.py
+++ aoc-2024/d04_a.py
@@ -4,31 +4,24 @@ import numpy as np
 puzzle = get_data(day=4, year=2024)
 grid = []
 for row in puzzle.split("\n"):
-    r = []
-    for x in row:
-        r.append(x)
-    grid.append(r)
+    grid.append([r for r in row])
 grid = np.array(grid)
-
 XMAS = 0
-for col in range(grid.shape[1]):
-    s = ''.join(grid[:, col])
-    XMAS += len(re.findall(r"XMAS", s))
-    XMAS += len(re.findall(r"SAMX", s))
-for row in range(grid.shape[0]):
-    s = ''.join(grid[row, :])
-    XMAS += len(re.findall(r"XMAS", s))
-    XMAS += len(re.findall(r"SAMX", s))
+def check(subs):
+    global XMAS
+    for s in subs:
+        XMAS += len(re.findall(r"XMAS", s))
+        XMAS += len(re.findall(r"SAMX", s))
+
+cols = [''.join(grid[:, col]) for col in range(grid.shape[1])]
+rows = [''.join(grid[row, :]) for row in range(grid.shape[1])]
 row, col = grid.shape
-diag = [grid.diagonal(x) for x in range(-row, col)]
-for d in diag:
-    s = ''.join(d)
-    XMAS += len(re.findall(r"XMAS", s))
-    XMAS += len(re.findall(r"SAMX", s))
+d1 = [''.join(grid.diagonal(x)) for x in range(-row, col)]
 flipped = np.fliplr(grid)
-diag = [flipped.diagonal(x) for x in range(-row, col)]
-for d in diag:
-    s = ''.join(d)
-    XMAS += len(re.findall(r"XMAS", s))
-    XMAS += len(re.findall(r"SAMX", s))
+d2 = [''.join(flipped.diagonal(x)) for x in range(-row, col)]
+
+check(cols)
+check(rows)
+check(d1)
+check(d2)
 print(XMAS)
--- aoc-2024/d04_b.py
+++ aoc-2024/d04_b.py
@@ -5,19 +5,18 @@ puzzle = get_data(day=4, year=2024)
 rows = [row for row in puzzle.split("\n")]
 XMAS = 0
 def check(pattern, x, y):
-    count = 0
+    global XMAS
     for m in re.finditer(pattern, row, overlapped=True):
         r1 = rows[i+1][m.start(): m.end()]
         r2 = rows[i+2][m.start(): m.end()]
         if r1[1] == "A" and r2[0] == x and r2[2] == y:
-            count += 1
-    return count
+            XMAS += 1
 
 for i in range(len(rows) - 2):
     row = rows[i]
-    XMAS += check(r"M.S", "M", "S")
-    XMAS += check(r"S.M", "S", "M")
-    XMAS += check(r"S.S", "M", "M")
-    XMAS += check(r"M.M", "S", "S")
+    check(r"M.S", "M", "S")
+    check(r"S.M", "S", "M")
+    check(r"S.S", "M", "M")
+    check(r"M.M", "S", "S")
 
 print(XMAS)
