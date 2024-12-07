f96f5c1db0053382b1b9ed0bf4da128d18710590
calle2021 <calle.janlenstenberg@gmail.com>
2024-12-07 13:44:13 +0100

    added solution to day 7 2024

---
+++ aoc-2024/d07_a.py
@@ -0,0 +1,22 @@
+from aocd import get_data
+
+puzzle = get_data(day=7, year=2024)
+
+def combs(i, v, e):
+    if i == len(e) - 1:
+        unique.append(v + e[i])
+        unique.append(v * e[i])
+        return
+    combs(i+1, v + e[i], e)
+    combs(i+1, v * e[i], e)
+
+tot = 0
+for eq in puzzle.split("\n"):
+    r, e = eq.split(":")
+    e = list(map(int, e.strip().split()))
+    r = int(r)
+    unique = []
+    combs(1, e[0], e)
+    if r in unique:
+        tot += r
+print(tot)
