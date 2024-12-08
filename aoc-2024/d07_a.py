328cc72b7025e413ab8daeddca26c29e34781cf2
calle2021 <calle.janlenstenberg@gmail.com>
2024-12-08 14:55:16 +0100

    clean up

--- aoc-2024/d07_a.py
+++ aoc-2024/d07_a.py
@@ -2,21 +2,20 @@ from aocd import get_data
 
 puzzle = get_data(day=7, year=2024)
 
-def combs(i, v, e):
-    if i == len(e) - 1:
-        unique.append(v + e[i])
-        unique.append(v * e[i])
+def equations(e, v):
+    if len(e) == 1:
+        eqs.append(v)
         return
-    combs(i+1, v + e[i], e)
-    combs(i+1, v * e[i], e)
+    equations(e[1:], v + e[1])
+    equations(e[1:], v * e[1])
 
-tot = 0
+res = 0
 for eq in puzzle.split("\n"):
     r, e = eq.split(":")
     e = list(map(int, e.strip().split()))
     r = int(r)
-    unique = []
-    combs(1, e[0], e)
-    if r in unique:
-        tot += r
-print(tot)
+    eqs = []
+    equations(e, e[0])
+    if r in eqs:
+        res += r
+print(res)
--- aoc-2024/d07_b.py
+++ aoc-2024/d07_b.py
@@ -1,23 +1,22 @@
 from aocd import get_data
 
 puzzle = get_data(day=7, year=2024)
-def combs(i, v, e):
-    if i == len(e) - 1:
-        unique.append(v + e[i])
-        unique.append(v * e[i])
-        unique.append(int(str(v) + str(e[i])))
-        return
-    combs(i+1, v + e[i], e)
-    combs(i+1, v * e[i], e)
-    combs(i+1, int(str(v) + str(e[i])), e)
 
-tot = 0
+def equations(e, v):
+    if len(e) == 1:
+        eqs.append(v)
+        return
+    equations(e[1:], v + e[1])
+    equations(e[1:], v * e[1])
+    equations(e[1:], int(str(v) + str(e[1])))
+
+res = 0
 for eq in puzzle.split("\n"):
     r, e = eq.split(":")
     e = list(map(int, e.strip().split()))
     r = int(r)
-    unique = []
-    combs(1, e[0], e)
-    if r in unique:
-        tot += r
-print(tot)
+    eqs = []
+    equations(e, e[0])
+    if r in eqs:
+        res += r
+print(res)
