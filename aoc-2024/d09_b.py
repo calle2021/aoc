a66595146999811ae6cd596687f012a416ed8d79
Calle Janlén Stenberg <calleja@axis.com>
2024-12-09 12:59:56 +0100

    added solution day 9 2024
    Change-Id: I78f9dde3a73987e8b7c1f25b1e61ceeb91b6ec36

--- aoc-2024/d09_b.py
+++ aoc-2024/d09_b.py
@@ -1,36 +1,36 @@
 from aocd import get_data
-import re
 
-puzzle = """2333133121414131402"""
-
-#puzzle = get_data(day=9, year=2024)
+puzzle = get_data(day=9, year=2024)
 
 puzzle = list(map(int, [x for x in puzzle]))
-system = []
 
-for i, x in  enumerate(puzzle):
+q = []
+free = []
+p = 0
+for i, x in enumerate(puzzle):
     if i % 2 == 0:
-        system = system + [str(i // 2)] * x
+        q.append((p, x))
     else:
-
-        system = system + ["."] * x
-
-files = [x.group() for x in re.finditer(r"(\d)\1*", ''.join(system))][::-1]
-free = [x.group() for x in re.finditer("\.+", ''.join(system))]
-
-print(files)
-print(free)
-exit()
-while files:
-    f = files.pop(0)
-    for i in range(len(free)):
-        if len(free[i]) >= len(f):
-            for j in range(len(f)):
-                free[i].pop()
-            
+        free.append((p, x))
+    p += x
+res = q.copy()
+i = len(q) - 1
+while i > 0:
+    start, number = q[i]
+    for j, f in enumerate(free):
+        if f[0] >= start:
+            break
+        if f[1] == number:
+            res[i] = (f[0], number)
+            free.pop(j)
+            break
+        if f[1] > number:
+            res[i] = (f[0], number)
+            free[j] = (f[0] + number, f[1] - number)
+            break
+    i -= 1
 checksum = 0
-for i, x in enumerate(system):
-    if x == ".":
-        continue
-    checksum += i * int(x)
+for i, r in enumerate(res):
+    for j in range(r[0], r[0] + r[1]):
+        checksum += j * i
 print(checksum)
