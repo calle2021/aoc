d15d6c27cf930e33ec736e603a5e49432c7a413d
calle2021 <calle.janlenstenberg@gmail.com>
2024-12-15 09:36:58 +0100

    added day 15 part 1 2024

---
+++ aoc-2024/d15_a.py
@@ -0,0 +1,54 @@
+from aocd import get_data
+
+puzzle = get_data(day=15, year=2024)
+
+grid, moves = puzzle.split("\n\n")
+m = {"<" : -1, ">" : 1, "^" : -1j, "v" : 1j}
+moves = moves.replace("\n", "")
+moves = [m[x] for x in moves.strip()]
+
+boxes = set()
+walls = set()
+
+for y, row in enumerate(grid.split("\n")):
+    for x, col in enumerate(row):
+        curr = x + y * 1j
+        if col == ".":
+            continue
+        if col == "#":
+            walls.add(curr)
+        if col == "O":
+            boxes.add(curr)
+        if col == "@":
+            robot =  curr
+def moveboxes(curr, dir):
+    q = []
+    while True:
+        curr += dir
+        if curr in walls:
+            q = []
+            break
+        if curr not in walls and curr not in boxes:
+            break
+        q.append(curr)
+    return q
+
+for i, move in enumerate(moves):
+    curr = robot + move
+    if curr not in walls and curr not in boxes:
+        robot += move
+        continue
+    if curr in walls:
+        continue
+    Q = moveboxes(robot, move)
+    if not Q: continue
+    robot += move
+    for q in Q:
+        boxes.remove(q)
+    for q in Q:
+        boxes.add(q + move)
+
+print(sum([int(b.real + b.imag * 100) for b in boxes]))
+
+
+
