2a831db623750f5143592a30edc5612cfa1e3a01
calle2021 <calle.janlenstenberg@gmail.com>
2024-12-16 20:25:08 +0100

    day 16 kinda working

--- aoc-2024/d16_b.py
+++ aoc-2024/d16_b.py
@@ -18,24 +18,24 @@ puzzle = """###############
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############"""
-# puzzle = """#################
-##...#...#...#..E#
-##.#.#.#.#.#.#.#.#
-##.#.#.#...#...#.#
-##.#.#.#.###.#.#.#
-##...#.#.#.....#.#
-##.#.#.#.#.#####.#
-##.#...#.#.#.....#
-##.#.#####.#.###.#
-##.#.#.......#...#
-##.#.###.#####.###
-##.#.#...#.....#.#
-##.#.#.#####.###.#
-##.#.#.........#.#
-##.#.#.#########.#
-##S#.............#
-##################"""
-# puzzle = get_data(day=16, year=2024)
+puzzle = """#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################"""
+puzzle = get_data(day=16, year=2024)
 
 paths = set()
 walls = set()
@@ -70,80 +70,62 @@ def print_grid():
                 print("E", end="")
                 continue
             if curr in paths:
-                print(".", end="")
+                print(" ", end="")
             if curr in walls:
-                print("#", end="")
+                print(" ", end="")
         print("")
 
-
-# print_grid()
-
-# Dijkstra
 q = []
 heappush(q, (0, (1, 0), start))
 cost_so_far = {}
 came_from = defaultdict(set)
-came_from[start] = None
-cost_so_far[start] = 0
+came_from[start].add(None)
+cost_so_far[((1, 0), start)] = 0
 while q:
     curr = heappop(q)
     cost, dir, pos = curr
-
     if pos == goal:
+        cost_so_far[goal] = cost
         break
 
     neighbours = []
     straight = (pos[0] + dir[0], pos[1] + dir[1])
-    left = (pos[0] + dir[1], pos[1] - dir[0])
-    right = (pos[0] - dir[1], pos[1] + dir[0])
     if straight in paths:
         neighbours.append((1, dir, straight))
-    if left in paths:
-        neighbours.append((1001, (dir[1], -dir[0]), left))
-    if right in paths:
-        neighbours.append((1001, (-dir[1], dir[0]), right))
+    neighbours.append((1000, (dir[1], -dir[0]), pos))
+    neighbours.append((1000, (-dir[1], dir[0]), pos))
 
     for next in neighbours:
-        cost, dir, pos = next
-        if pos not in paths:
-            continue
-        new_cost = cost_so_far[curr[2]] + cost
-        if pos not in cost_so_far or new_cost < cost_so_far[pos]:
-            cost_so_far[pos] = new_cost
-            heappush(q, (new_cost, dir, pos))
-            came_from[pos] = {curr[2]}
-        if pos not in cost_so_far or new_cost == cost_so_far[pos]:
-            cost_so_far[pos] = new_cost
-            heappush(q, (new_cost, dir, pos))
-            came_from[pos].add(curr[2])
-            if pos == (3, 9):
-                print(curr[2], "here")
+        ncost, ndir, npos = next
+        new_cost = cost_so_far[(dir, pos)] + ncost
+        if (ndir, npos) not in cost_so_far or new_cost <= cost_so_far[(ndir, npos)]:
+            cost_so_far[(ndir, npos)] = new_cost
+            heappush(q, (new_cost, ndir, npos))
+            came_from[npos].add(pos)
 
 path = []
 print_grid()
 
-print(came_from[(3, 9)])
-print(cost_so_far[(3, 9)])
-exit()
-print("PART 1", cost_so_far[goal])
+print(cost_so_far[goal])
+
 q = [goal]
 visited = set()
 path = []
-print(came_from[goal])
 while q:
     curr = q.pop(0)
     if curr == None or came_from[curr] == None:
         break
-    neighbors = [x for x in came_from[curr]]
-    if curr == (5, 7):
-        print(curr, neighbors)
-        time.sleep(1)
+    neighbors = []
+    for x in came_from[curr]:
+        if x == curr:
+            continue
+        neighbors.append(x)
     for next in neighbors:
         if next in visited:
             continue
         q.append(next)
+        visited.add(next)
         path.append(next)
 print_grid()
+print(len(path))
 
-
-print(came_from[(5, 7)])
