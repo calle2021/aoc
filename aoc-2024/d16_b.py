f23290b1eceb7bfaf0f952ab0685fce1d8ed4508
Calle Janlén Stenberg <calleja@axis.com>
2024-12-16 16:55:48 +0100

    progress day 16 b
    Change-Id: I9f92fa64d51109e611218a180cee06287927e3be

--- aoc-2024/d16_b.py
+++ aoc-2024/d16_b.py
@@ -1,6 +1,8 @@
 from aocd import get_data
 from heapq import *
 from collections import defaultdict
+import time
+
 puzzle = """###############
 #.......#....E#
 #.#.###.#.###.#
@@ -16,24 +18,24 @@ puzzle = """###############
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############"""
-#puzzle = """#################
+# puzzle = """#################
 ##...#...#...#..E#
-##.#.#.#.#.#.#.#^#
-##.#.#.#...#...#^#
-##.#.#.#.###.#.#^#
-##>>v#.#.#.....#^#
-##^#v#.#.#.#####^#
-##^#v..#.#.#>>>>^#
-##^#v#####.#^###.#
-##^#v#..>>>>^#...#
-##^#v###^#####.###
-##^#v#>>^#.....#.#
-##^#v#^#####.###.#
-##^#v#^........#.#
-##^#v#^#########.#
-##S#>>^..........#
+##.#.#.#.#.#.#.#.#
+##.#.#.#...#...#.#
+##.#.#.#.###.#.#.#
+##...#.#.#.....#.#
+##.#.#.#.#.#####.#
+##.#...#.#.#.....#
+##.#.#####.#.###.#
+##.#.#.......#...#
+##.#.###.#####.###
+##.#.#...#.....#.#
+##.#.#.#####.###.#
+##.#.#.........#.#
+##.#.#.#########.#
+##S#.............#
 ##################"""
-#puzzle = get_data(day=16, year=2024)
+# puzzle = get_data(day=16, year=2024)
 
 paths = set()
 walls = set()
@@ -52,9 +54,10 @@ for y, row in enumerate(puzzle.split("\n")):
             start = curr
         if col == "E":
             goal = curr
-        
+
+
 def print_grid():
-    for y in range(my+1):
+    for y in range(my + 1):
         for x in range(mx + 1):
             curr = (x, y)
             if curr in path:
@@ -71,54 +74,76 @@ def print_grid():
             if curr in walls:
                 print("#", end="")
         print("")
-#print_grid()
 
-#Dijkstra
-front = []
-heappush(front, (0, (1, 0), start))
+
+# print_grid()
+
+# Dijkstra
+q = []
+heappush(q, (0, (1, 0), start))
 cost_so_far = {}
-came_from = defaultdict(list)
-came_from[start].append(None)
-cost_so_far[(start, (1,0))] = 0
-while front:
-    curr = heappop(front)
+came_from = defaultdict(set)
+came_from[start] = None
+cost_so_far[start] = 0
+while q:
+    curr = heappop(q)
     cost, dir, pos = curr
 
     if pos == goal:
-        cost_so_far[pos] = cost
         break
 
     neighbours = []
-    if (pos[0] + dir[0], pos[1] + dir[1]) in paths:
-        neighbours.append((1, dir, (pos[0] + dir[0], pos[1] + dir[1])))
-    neighbours.append((1000, (dir[1], -dir[0]), pos))
-    neighbours.append((1000, (-dir[1], dir[0]), pos))
+    straight = (pos[0] + dir[0], pos[1] + dir[1])
+    left = (pos[0] + dir[1], pos[1] - dir[0])
+    right = (pos[0] - dir[1], pos[1] + dir[0])
+    if straight in paths:
+        neighbours.append((1, dir, straight))
+    if left in paths:
+        neighbours.append((1001, (dir[1], -dir[0]), left))
+    if right in paths:
+        neighbours.append((1001, (-dir[1], dir[0]), right))
+
     for next in neighbours:
         cost, dir, pos = next
         if pos not in paths:
             continue
-        new_cost = cost_so_far[(curr[2], curr[1])] + cost
-        if (pos, dir) not in cost_so_far or new_cost < cost_so_far[(pos, dir)]:
-            cost_so_far[(pos, dir)] = new_cost
-            priority = new_cost
-            heappush(front, (priority, dir, pos))
-            came_from[pos].append(curr[2])
-print(cost_so_far[goal])
-print("here",came_from[goal])
+        new_cost = cost_so_far[curr[2]] + cost
+        if pos not in cost_so_far or new_cost < cost_so_far[pos]:
+            cost_so_far[pos] = new_cost
+            heappush(q, (new_cost, dir, pos))
+            came_from[pos] = {curr[2]}
+        if pos not in cost_so_far or new_cost == cost_so_far[pos]:
+            cost_so_far[pos] = new_cost
+            heappush(q, (new_cost, dir, pos))
+            came_from[pos].add(curr[2])
+            if pos == (3, 9):
+                print(curr[2], "here")
 
+path = []
+print_grid()
+
+print(came_from[(3, 9)])
+print(cost_so_far[(3, 9)])
+exit()
+print("PART 1", cost_so_far[goal])
 q = [goal]
 visited = set()
-visited.add(goal)
-count = 0
-path = [goal]
+path = []
+print(came_from[goal])
 while q:
     curr = q.pop(0)
-    neighbours = came_from[curr]
-    for next in neighbours:
-        if next not in visited:
-            q.append(next)
-            visited.add(next)
-            count += 1
-            path.append(next)
+    if curr == None or came_from[curr] == None:
+        break
+    neighbors = [x for x in came_from[curr]]
+    if curr == (5, 7):
+        print(curr, neighbors)
+        time.sleep(1)
+    for next in neighbors:
+        if next in visited:
+            continue
+        q.append(next)
+        path.append(next)
 print_grid()
-print(count)
+
+
+print(came_from[(5, 7)])
