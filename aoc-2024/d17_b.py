0ddabc7bf4b4ad3a8ff3b90a104a83c317c1c40f
Calle Janlén Stenberg <calleja@axis.com>
2024-12-18 08:18:58 +0100

    added solution day 17 2024
    Change-Id: I08626fdf16fbf034ad920126f672da35c5de47b2

--- aoc-2024/d17_b.py
+++ aoc-2024/d17_b.py
@@ -13,12 +13,13 @@ def program(a):
             break
     return output
 
-prog = [2,4,1,2,7,5,4,1,1,3,5,5,0,3,3,0]
 
-prog.pop()
+prog = [2, 4, 1, 2, 7, 5, 4, 1, 1, 3, 5, 5, 0, 3, 3, 0]
+
 i = 1
-tail = [0]
+tail = [prog.pop()]
 for curr in prog[::-1]:
+    tail = [curr] + tail
     while True:
         a = i
         b = a % 8
@@ -28,9 +29,7 @@ for curr in prog[::-1]:
         b = b ^ 3
         out = b % 8
         a = a >> 3
-        if out == curr and a != 0 and program(i) == [curr] + tail:
-            o = program(i)
-            tail.insert(0, curr)
+        if out == curr and a != 0 and program(i) == tail:
             i = i << 3
             break
         i += 1
