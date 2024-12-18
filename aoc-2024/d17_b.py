861133a336fa2cac12d8c19f2f5757c462036d50
calle2021 <calle.janlenstenberg@gmail.com>
2024-12-18 07:28:22 +0100

    added solution day 17 2024

--- aoc-2024/d17_b.py
+++ aoc-2024/d17_b.py
@@ -1,8 +1,23 @@
+def program(a):
+    output = []
+    while True:
+        b = a % 8
+        b = b ^ 2
+        c = a >> b
+        b = b ^ c
+        b = b ^ 3
+        out = b % 8
+        output.append(out)
+        a = a >> 3
+        if a == 0:
+            break
+    return output
+
 prog = [2,4,1,2,7,5,4,1,1,3,5,5,0,3,3,0]
 
 prog.pop()
 i = 1
-candidates = []
+tail = [0]
 for curr in prog[::-1]:
     while True:
         a = i
@@ -13,26 +28,10 @@ for curr in prog[::-1]:
         b = b ^ 3
         out = b % 8
         a = a >> 3
-        if out == curr and a != 0:
-            print(i, b, c)
+        if out == curr and a != 0 and program(i) == [curr] + tail:
+            o = program(i)
+            tail.insert(0, curr)
             i = i << 3
             break
         i += 1
-
-prog = [2,4,1,2,7,5,4,1,1,3,5,5,0,3,3,0]
-
-a = 549
-output = []
-while True:
-    b = a % 8
-    b = b ^ 2
-    c = a >> b
-    b = b ^ c
-    b = b ^ 3
-    out = b % 8
-    output.append(out)
-    a = a >> 3
-    if a == 0:
-        break
-print(b, c)
-print(output)
+print(i >> 3)
