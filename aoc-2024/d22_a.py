c178b159df00cffa7179c5014a11c986387d74f4
calle2021 <calle.janlenstenberg@gmail.com>
2024-12-22 11:52:05 +0100

    added solution day 22 2024

--- aoc-2024/d22_a.py
+++ aoc-2024/d22_a.py
@@ -5,7 +5,7 @@ puzzle = get_data(day=22, year=2024)
 numbers = list(map(int, puzzle.split("\n")))
 for _ in range(2000):
     for i, n in enumerate(numbers):
-        numbers[i] = ((numbers[i] ^ numbers[i] * 64)) % 16777216
-        numbers[i] = ((numbers[i] ^ numbers[i] // 32)) % 16777216
-        numbers[i] = ((numbers[i] ^ numbers[i] * 2048)) % 16777216
+        numbers[i] = (numbers[i] ^ (numbers[i] * 64)) % 16777216
+        numbers[i] = (numbers[i] ^ (numbers[i] // 32)) % 16777216
+        numbers[i] = (numbers[i] ^ (numbers[i] * 2048)) % 16777216
 print(sum(numbers))
