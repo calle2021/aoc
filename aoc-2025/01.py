a95a9d99a88a9176d8eb61648775c99c0a11f12a
calle2021 <calle.janlenstenberg@gmail.com>
2025-12-01 06:20:28 +0100

    Add solution 01 2025

--- aoc-2025/01.py
+++ aoc-2025/01.py
@@ -1,4 +1,26 @@
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2025, day=1).input_data
-data = puzzle.split("\n")
-print(data)
+curr = 50
+ans = 0
+ans2 = 0
+for line in puzzle.splitlines():
+    d = line[0]
+    clicks = int(line[1:])
+    for click in range(clicks):
+        if curr == 0 and click > 0:
+            ans2 += 1
+        if d == "L":
+            if curr == 0:
+                curr = 99
+            else:
+                curr -= 1
+        if d == "R":
+            if curr == 99:
+                curr = 0
+            else:
+                curr += 1
+    if curr == 0:
+        ans += 1
+        ans2 += 1
+
+print(ans, ans2)
