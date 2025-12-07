0a5813e421e74b7334835b6bafd2eb105f57084a
calle2021 <calle.janlenstenberg@gmail.com>
2025-12-07 20:13:29 +0100

    Clean up 05

--- aoc-2025/05_a.py
+++ aoc-2025/05_a.py
@@ -1,24 +1,13 @@
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2025, day=5).input_data
-ranges = set()
-numbers = set()
-for window in puzzle.splitlines():
-    if not len(window): continue
-    if "-" in window:
-        lo, hi = window.split("-")
-        ranges.add((int(lo), int(hi)))
-    else:
-        numbers.add(int(window))
+ranges, numbers = puzzle.split("\n\n")
+ranges = ranges.split("\n")
+ranges = [eval(x.replace("-", ",")) for x in ranges]
+numbers = list(map(int, numbers.split("\n")))
 fresh = 0
-def inside(number, window):
-    if number >= window[0] and number <= window[1]:
-        return True
-    return False
 for number in numbers:
-    isfresh = False
     for window in ranges:
-        if inside(number, window):
-            isfresh = True
+        if number >= window[0] and number <= window[1]:
+            fresh += 1
             break
-    fresh += 1 if isfresh else 0
 print(fresh)
--- aoc-2025/05_b.py
+++ aoc-2025/05_b.py
@@ -1,50 +1,17 @@
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2025, day=5).input_data
-
-ranges = []
-for r in puzzle.splitlines():
-    if not len(r): continue
-    if "-" in r:
-        lo, hi = r.split("-")
-        ranges.append((int(lo), int(hi)))
-
-def merge(window, data):
-    i = 0
-    if window in data:
-        i = data.index(window)
-        data.pop(i)
-    for j, w in enumerate(data):
-        lo = w[0]
-        hi = w[1]
-        update = False
-        if window[0] >= w[0] and window[0] <= w[1]:
-            hi = max(w[1], window[1])
-            update = True
-        if window[1] >= w[0] and window[1] <= w[1]:
-            lo = min(window[0], w[0])
-            update = True
-        if update:
-            data[j] = (lo, hi)
-            return data[j]
-    data.insert(i, window)
-    return window
-
-merged = []
-for window in ranges:
-    while True:
-        res = merge(window, merged)
-        if res == window:
-            break
-        window = res
-
-for window in merged:
-    while True:
-        res = merge(window, merged)
-        if res == window:
-            break
-        window = res
-
-fresh = 0
-for win in merged:
-    fresh += win[1] - win[0] + 1
+ranges, _ = puzzle.split("\n\n")
+ranges = ranges.split("\n")
+ranges = [eval(x.replace("-", ",")) for x in ranges]
+ranges = sorted(ranges)
+i = 0
+while i < len(ranges) - 1:
+    curr = ranges[i]
+    next = ranges[i + 1]
+    if next[0] <= curr[1]:
+        ranges[i] = (curr[0], max(curr[1], next[1]))
+        ranges.pop(i + 1)
+        continue
+    i += 1
+fresh = sum([(x[1] - x[0]) + 1 for x in ranges])
 print(fresh)
