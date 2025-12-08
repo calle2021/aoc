9fea78c58d6d045bdd6602d868a8dc7dd21ff149
Calle Janlén Stenberg <calleja@lap5cg1163t06.se.axis.com>
2025-12-08 07:32:04 +0100

    add 08 progress

--- aoc-2025/08_a.py
+++ aoc-2025/08_a.py
@@ -20,30 +20,48 @@ puzzle = """162,817,812
 862,61,35
 984,92,344
 425,690,689"""
-#puzzle = Puzzle(year=2025, day=8).input_data
+puzzle = Puzzle(year=2025, day=8).input_data
 junktions = [eval(x) for x in puzzle.splitlines()]
 connections = []
 dists = []
 pairs = []
+def euc(x, y):
+    return np.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2 + (x[2] - y[2]) ** 2)
 for i, x in enumerate(junktions):
     for j, y in enumerate(junktions):
         if j == i: continue
-        dist = np.linalg.norm(np.array(x) - np.array(y))
+        add = sorted((x, y))
+        if add in pairs: continue
+        dist = euc(x, y)
         dists.append(dist)
-        pairs.append((x, y))
-
+        pairs.append(add)
 pairs = [x for _, x in sorted(zip(dists, pairs))]
-
-for pair in pairs:
-    x, y = pair
-    add = False
-    for c in connections:
+dists = sorted(dists)
+for k in range(10):
+    x, y = pairs[k]
+    ids = []
+    for i, c in enumerate(connections):
         if x in c or y in c:
-            c.append(x)
-            c.append(y)
-            add = True
-            break
-    if not add:
-        connections.append([x, y])
-
-print(len(connections))
+            ids.append(i)
+    if not ids:
+        connections.append({x ,y})
+        continue
+    i = ids[0]
+    if len(ids) == 1:
+        connections[i].add(x)
+        connections[i].add(y)
+        continue
+    connections[i].add(x)
+    connections[i].add(y)
+    updated = []
+    circ = set()
+    for j in ids:
+        circ |= connections[j]
+    for j in range(len(connections)):
+        if j in ids:
+            continue
+        updated.append(connections[j])
+    updated.append(circ)
+    connections = updated
+lens = sorted([len(x) for x in connections])
+print(lens[-1] * lens[-2] * lens[-3])
