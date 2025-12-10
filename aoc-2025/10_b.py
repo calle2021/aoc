1195bdd970a62fc901555224132dd68aa93f7c89
Calle Janlén Stenberg <calleja@axis.com>
2025-12-10 16:27:03 +0100

    10 b clean up

--- aoc-2025/10_b.py
+++ aoc-2025/10_b.py
@@ -1,5 +1,6 @@
 from aocd.models import Puzzle
 from functools import cache
+import re
 puzzle = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
@@ -7,39 +8,35 @@ puzzle = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 
 #puzzle = Puzzle(year=2025, day=10).input_data
 
-data = []
-for line in puzzle.splitlines():
-    schematics = line.split(" ")
-    lights = schematics[0].replace("[", "").replace("]", "")
-    lights = tuple([0 if x == "." else 1 for x in lights])
-    buttons = tuple([eval(x) for x in schematics[1:-1]])
-    joltage = tuple(eval(schematics[-1].replace("{", "[").replace("}", "]")))
-    data.append((joltage, buttons))
+manual = []
+for instruction in puzzle.splitlines():
+    instruction = instruction.split(" ")
+    buttons = [eval(x) for x in instruction[1:-1]]
+    joltage = tuple(map(int, re.findall(r"\d+", instruction[-1])))
+    manual.append([buttons, joltage])
 
 @cache
-def press(curr, presses, target):
-    if curr == target:
+def press(curr, presses):
+    if curr == joltage:
         return presses
-    if any([x > y for x, y in zip(curr, target)]):
+    if any([x > y for x, y in zip(curr, joltage)]):
         return float("inf")
     fewest = float("inf")
     for button in buttons:
-        new = list(curr)
+        next = list(curr)
         if isinstance(button, int):
-            new[button] += 1
+            next[button] += 1
         else:
-            for i in range(len(curr)):
-                if i in button:
-                    new[i] += 1
-        fewest = min(fewest, press(tuple(new), presses + 1, target))
+            for b in button:
+                next[b] += 1
+        fewest = min(fewest, press(tuple(next), presses + 1))
     return fewest
 
-ans = 0
-for d in data:
-    state = [0 for _ in range(len(d[0]))]
-    print(tuple(state), 0, d[1], d[0])
-    buttons = d[1]
-    few = press(tuple(state), 0, d[0])
-    ans += few
+fewest = 0
+for buttons, joltage in manual:
+    curr = tuple([0 for _ in joltage])
+    few = press(curr, 0)
+    fewest += few
     print(few)
-print(ans)
+
+print(fewest)
