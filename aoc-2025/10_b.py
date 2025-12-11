16a638233fdb2b73828685e12ea35e8727648eda
calle2021 <calle.janlenstenberg@gmail.com>
2025-12-11 21:28:10 +0100

    Add 10 b package solution

--- aoc-2025/10_b.py
+++ aoc-2025/10_b.py
@@ -1,48 +1,25 @@
 from aocd.models import Puzzle
+from scipy.optimize import LinearConstraint
+from scipy.optimize import milp
+import numpy as np
 import re
-puzzle = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
-
-
 puzzle = Puzzle(year=2025, day=10).input_data
-
 manual = []
 for instruction in puzzle.splitlines():
     instruction = instruction.split(" ")
-    buttons = [eval(x) for x in instruction[1:-1]]
-    joltage = tuple(map(int, re.findall(r"\d+", instruction[-1])))
+    buttons = [[eval(x)] if isinstance(eval(x), int) else list(eval(x)) for x in instruction[1:-1]]
+    joltage = [int(x) for x in re.findall(r"\d+", instruction[-1])]
     manual.append([buttons, joltage])
 
-
-def press(curr, presses):
-    if (curr, presses) in cache:
-        return cache[(curr, presses)]
-    if curr == joltage:
-        return presses
-    if any([x > y for x, y in zip(curr, joltage)]):
-        return float("inf")
-    fewest = float("inf")
-    for button in buttons:
-        next = list(curr)
-        if isinstance(button, int):
-            next[button] += 1
-        else:
-            for b in button:
-                next[b] += 1
-        fewest = min(fewest, press(tuple(next), presses + 1))
-    cache[((curr, presses))] = fewest
-    return fewest
-
-fewest = 0
+presses = 0
 for buttons, joltage in manual:
-    if max(joltage) > 40:
-        continue
-    print(buttons, joltage)
-    cache = {}
-    curr = tuple([0 for _ in joltage])
-    few = press(curr, 0)
-    fewest += few
-    print(few)
-
-print(fewest)
+    nrows = len(joltage)
+    ncols = len(buttons)
+    M = np.column_stack([np.array([1 if x in button else 0 for x in range(nrows)]) for button in buttons])
+    c_lo = c_hi = np.array(joltage)
+    cons = LinearConstraint(M, c_lo, c_hi)
+    coeffs = np.ones(ncols)
+    integrality = np.ones_like(coeffs)
+    solution = milp(c = coeffs, constraints = cons, integrality = integrality)
+    presses += sum([int(round(x)) for x in solution.x])
+print(presses)
